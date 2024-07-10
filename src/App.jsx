87749aef1b6e8ef24ca2c01f6d561433d5f309fd
Arun Kumar R <aruntutter19@gmail.com>
2024-07-10 17:38:56 +0530

    context API setup

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,21 @@ import Home from "./components/home/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import PostHome from "./components/postHome/PostHome";
 import TrackingScreen from "./components/trackingScreen/TrackingScreen";
+import myState from "./context/myState";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/posthome" element={<PostHome />} />
-        <Route path="/tracking" element={<TrackingScreen />} />
-      </Routes>
-    </Router>
+    <myState>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/posthome" element={<PostHome />} />
+          <Route path="/tracking" element={<TrackingScreen />} />
+        </Routes>
+      </Router>
+    </myState>
   );
 };
 
