f7650913c59b73da3243aaf04bf2d75f01574372
Arun Kumar R <aruntutter19@gmail.com>
2024-07-10 17:38:40 +0530

    firebase installed and configured

---
+++ src/firebase/FirebaseConfig.jsx
@@ -0,0 +1,20 @@
+// Firebase Config
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBxziw0lb2L7_tE6H-0JBCHIKZevbok2wo",
+  authDomain: "chrono-track-4f4a5.firebaseapp.com",
+  projectId: "chrono-track-4f4a5",
+  storageBucket: "chrono-track-4f4a5.appspot.com",
+  messagingSenderId: "347632019486",
+  appId: "1:347632019486:web:cda4140ca3fc84bd21a60f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const fireDb = getAuth(app);
+
+export { app, auth, fireDb };
